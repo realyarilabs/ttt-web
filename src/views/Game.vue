25547c10399969e898b96e0b7b66b555de041483
Pedro Lima <pedro@yarilabs.com>
2024-01-31 14:15:25 +0000

    initial game board

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,21 +1,31 @@
 <template>
-  here is the game
-  {{ phoenixSocketStore.gameState }}
+  <div class="h-screen w-screen flex items-center justify-center bg-ttt">
+    <div class="flex flex-col justify-center items-center z-10">
+      <h1 class="ttt-h1 pb-2">GAME ON!</h1>
+      <p class="ttt-subtext">Game code</p>
+      <div v-if="phoenixSocketStore.gameState" class="flex flex-col justify-center items-center pt-[3.69rem]">
+        <div
+          v-for="(row, i) of phoenixSocketStore.gameState.board"
+          :key="i"
+          class="flex gap-[0.94rem] pb-[0.94rem]"
+        >
+          <button
+            v-for="(value, j) of row"
+            :key="j"
+            class="ttt-square w-[8.125rem] h-[8.125rem] rounded-[0.625rem]"
+            @click="phoenixSocketStore.executeGameMove(i, j)"
+          >
+            {{ value || "*" }}
+          </button>
+        </div>
+      </div>
+  </div>
 
-  <div v-if="phoenixSocketStore.gameState">
-    <div
-      v-for="(row, i) of phoenixSocketStore.gameState.board"
-      :key="i"
-      class="flex gap-2"
-    >
-      <button
-        v-for="(value, j) of row"
-        :key="j"
-        @click="phoenixSocketStore.executeGameMove(i, j)"
-      >
-        {{ value || "*" }}
-      </button>
-    </div>
+    <!--div >
+
+    here is the game
+    {{ phoenixSocketStore.gameState }}
+    </div-->
   </div>
 </template>
 
@@ -57,3 +67,9 @@ watch(
   { immediate: true, deep: true }
 );
 </script>
+
+<style>
+.ttt-square{
+  background: var(--Game-Board-BG, rgba(45, 128, 120, 0.20));
+}
+</style>
