ae7821a66ed63d3e038049b4a7b7d81b8f8817d4
Pedro Lima <pedro@yarilabs.com>
2024-01-31 10:59:46 +0000

    add css for btns + colors

--- src/App.vue
+++ src/App.vue
@@ -8,3 +8,41 @@ import { usePhoenixSocketStore } from "./stores/phoenixSocketStore";
 usePhoenixSocketStore();
 </script>
 
+<style>
+:root{
+  --bg-color: #1C404C;
+  --Light-Text: #FFF;
+  --Light-Text-2: #F4D9AE;
+  --CTA: #CD4F41;
+  --CTA-2: #DE9E46;
+}
+
+.bg-ttt{
+  background: var(--bg-color);
+}
+
+.ttt-button{
+  display: flex;
+  width: 20rem;
+  padding: 1.125rem 1.5rem;
+  justify-content: center;
+  align-items: center;
+  gap: 0.625rem;
+  border-radius: 0.375rem;
+  /* Text */
+  font-family: "Noto Sans";
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+}
+
+.btn-1{
+  background: var(--CTA);
+  color: var(--Light-Text);
+}
+.btn-2{
+  background: var(--CTA-2);
+  color: var(--Light-Text);
+}
+</style>
