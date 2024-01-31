f88d48a3fe46a1ada2373e4eb8de8d5500786fab
Pedro Lima <pedro@yarilabs.com>
2024-01-31 11:00:06 +0000

    add Home styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="h-screen w-screen flex items-center justify-center">
+  <div class="h-screen w-screen flex items-center justify-center bg-ttt">
     <div class="flex flex-col justify-center items-center">
-      <div>TIC TAC TOE CHALLENGE !</div>
-      <div>Ready for a game of Xs and Os ?</div>
-
-      <button @click="createGame(true)">CREATE NEW GAME</button>
-      <span> OR</span>
-      <input v-model="phoenixSocketStore.gameID" placeholder="Enter Game ID" />
-      <button @click="createGame()">JOIN EXISTING GAME</button>
+      <h1 class="ttt-h1 pb-2">TIC TAC TOE CHALLENGE !</h1>
+      <p class="ttt-subtext">Ready for a game of Xs and Os ?</p>
+      <div class="flex flex-col justify-center items-center pt-[3.69rem]">
+        <button class="ttt-button btn-1" @click="createGame(true)">CREATE NEW GAME</button>
+        <span class="py-5 ttt-or">OR</span>
+        <input class="flex items-center self-stretch ttt-input rounded-md" v-model="phoenixSocketStore.gameID" placeholder="Enter Game ID" />
+        <button class="ttt-button btn-2 mt-[2.12rem]" @click="createGame()">JOIN EXISTING GAME</button>
+      </div>
   </div>
  </div>
 </template>
@@ -31,3 +32,56 @@ const createGame = (createGameId?: boolean) => {
   }
 };
 </script>
+
+<style>
+.ttt-h1{
+  color: var(--Light-Text);
+  font-family: "Noto Sans";
+  font-size: 2.375rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+}
+
+.ttt-subtext{
+  color: var(--Light-Text-2);
+  font-family: Gilroy-Light;
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+
+.ttt-or{
+  color: var(--Light-Text);
+  font-family: "Noto Sans";
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+}
+
+.ttt-input{
+  padding: 1.125rem 0rem 1.125rem 1.5rem;
+  gap: 0.625rem;
+  border: 1px solid var(--Light-Text-2);
+  background: var(--bg-color);
+  /* Text */
+  color: var(--Light-Text-2);
+  font-family: Gilroy-Light;
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+
+.ttt-input::placeholder {
+  /* Text */
+  color: var(--Light-Text-2);
+  font-family: Gilroy-Light;
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+</style>
