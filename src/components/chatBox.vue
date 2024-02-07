5f2aaba8847ee5bb9ea1da948162069adf9cb450
Pedro Lima <pedro@yarilabs.com>
2024-02-07 13:39:48 +0000

    chatbox exercises

--- src/components/chatBox.vue
+++ src/components/chatBox.vue
@@ -4,50 +4,56 @@
 			id="chat"
 			class="flex flex-col w-full bg-[#2d8078] rounded-t-lg h-[75%] max-h-[310px] overflow-y-auto scrollbar-thumb-rounded"
 		>
-			<div v-for="(message, index) in chatHistoryAdvanced" :key="index" class="px-2">
-				<span v-if="checkIsSpectator(message.sender_id)" class="text-black">{{ message.message }}</span>
-				<span v-else class="text-white">{{ message.message }}</span>
+			<div v-for="(message, index) in chatHistory" :key="index" class="px-2">
+				<span class="text-white">{{ message }}</span>
 			</div>
 		</div>
 		<div class="flex flex-col h-[25%]">
-			<input class="flex items-center self-stretch ttt-input-2 rounded-none p-2" v-model="currentMessage" />
+			<input class="flex items-center self-stretch ttt-input-2 rounded-none p-2" v-model="currentMessage" @keypress.enter="sendMessage(currentMessage)" />
 			<button class="ttt-button btn-2 rounded-none" @click="sendMessage(currentMessage)">SEND</button>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-	import { ref, onMounted } from "vue"
+	import { ref, onMounted,computed } from "vue"
 	import { useTicTacToeStore } from "../stores/ticTacToeStore"
-	 import { useTicTacToeHelpers } from "../composables/tttHelper";
+  // #4
+  // For advanced exercise uncomment these lines
+  // save the whole payload into chatHistoryAdvanced and use checkIsSpectator in template to distinguish between players/spectators
+	//import { useTicTacToeHelpers } from "../composables/tttHelper";
+	//const { checkIsSpectator } = useTicTacToeHelpers()
+	//interface ChatMessage { message: string; sender_id: string;}
+	//const chatHistoryAdvanced = ref(Array<ChatMessage>())
 
-	const { checkIsSpectator } = useTicTacToeHelpers()
 	const ticTacToeStore = useTicTacToeStore()
 
 	const chatHistory = ref([""])
 	const currentMessage = ref("")
 
-	interface ChatMessage { message: string; sender_id: string;}
-	const chatHistoryAdvanced = ref(Array<ChatMessage>())
+
+  const matchChannel = computed(() => {
+     // #1
+    //return the match channel from ticTacToeStore
+  })
+
+  // To access ref and computed properties tou must use var_name.value
 
 	const sendMessage = (message: string) => {
-		ticTacToeStore.matchChannel?.push("broadcast_message", { message })
+     // #2
+    //push to the match channel the "broadcast_message" event with the message as payload
 	}
 
 	onMounted(() => {
-		ticTacToeStore.matchChannel?.on("game_message", (payload: { message: string; sender_id: string }) => {
-      chatHistory.value.push(payload.message)
-			chatHistoryAdvanced.value.push(payload)
-	     autoScroll()
-		})
+     // #3
+    //listen to the "game_message" event from match channel with the message as payload: { message: string; sender_id: string } and push the message to chat history
+    //optional: you can also use autoScroll function to scroll to most recent message
 	})
 
 	const autoScroll = () => {
 		setTimeout(() => {
 			var objDiv = document.querySelector("#chat > div:last-of-type")
-			if (objDiv) {
-				objDiv.scrollIntoView()
-			}
+			if (objDiv) objDiv.scrollIntoView()
 		}, 250)
 	}
 </script>
@@ -55,10 +61,10 @@
 <style>
 	.ttt-input-2 {
 		gap: 0.625rem;
-		border: 1px solid var(--Light-Text-2);
-		background: var(--bg-color);
+		border: 1px solid var(--bg-color);
+		background: white;
 		/* Text */
-		color: var(--Light-Text-2);
+		color: var(--bg-color);
 		font-family: Gilroy-Light;
 		font-size: 1.5rem;
 		font-style: normal;
