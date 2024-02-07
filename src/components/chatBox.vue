49c097548e403f3be4de14f10caaa25b702f2d44
Pedro Lima <pedro@yarilabs.com>
2024-02-07 12:38:07 +0000

    add auto scroll on chat

--- src/components/chatBox.vue
+++ src/components/chatBox.vue
@@ -1,7 +1,10 @@
 <template>
 	<div class="flex flex-col size-full w-full overflow-hidden grow-0">
-		<div class="flex flex-col w-full bg-[#2d8078] rounded-t-lg h-[75%] max-h-[75%] overflow-y-scroll grow-0">
-			<div v-for="(message, index) in chatHistory" :key="index" class=" px-2">
+		<div
+			id="chat"
+			class="flex flex-col w-full bg-[#2d8078] rounded-t-lg h-[75%] max-h-[310px] overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
+		>
+			<div v-for="(message, index) in chatHistory" :key="index" class="px-2">
 				{{ message }}
 			</div>
 		</div>
@@ -26,11 +29,21 @@
 	}
 
 	onMounted(() => {
-		ticTacToeStore.matchChannel?.on("game_message", (payload: {message :string, sender_id:string}) => {
-			console.log(payload)
+		ticTacToeStore.matchChannel?.on("game_message", (payload: { message: string; sender_id: string }) => {
 			chatHistory.value.push(payload.message)
+      autoScroll()
 		})
 	})
+
+	const autoScroll = () => {
+		setTimeout(() => {
+			var objDiv = document.querySelector("#chat > div:last-of-type") // document.getElementById("your_div");
+			if (objDiv) {
+				objDiv.scrollIntoView()
+			}
+		}, 250)
+	}
+
 </script>
 
 <style>
