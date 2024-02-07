9fb9fc505ac8fce452ff4daea728f75a293ddae2
Pedro Lima <pedro@yarilabs.com>
2024-02-07 13:26:02 +0000

    advanced task

--- src/components/chatBox.vue
+++ src/components/chatBox.vue
@@ -2,10 +2,11 @@
 	<div class="flex flex-col size-full w-full overflow-hidden grow-0">
 		<div
 			id="chat"
-			class="flex flex-col w-full bg-[#2d8078] rounded-t-lg h-[75%] max-h-[310px] overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
+			class="flex flex-col w-full bg-[#2d8078] rounded-t-lg h-[75%] max-h-[310px] overflow-y-auto scrollbar-thumb-rounded"
 		>
-			<div v-for="(message, index) in chatHistory" :key="index" class="px-2 text-white">
-				{{ message }}
+			<div v-for="(message, index) in chatHistoryAdvanced" :key="index" class="px-2">
+				<span v-if="checkIsSpectator(message.sender_id)" class="text-black">{{ message.message }}</span>
+				<span v-else class="text-white">{{ message.message }}</span>
 			</div>
 		</div>
 		<div class="flex flex-col h-[25%]">
@@ -18,11 +19,16 @@
 <script setup lang="ts">
 	import { ref, onMounted } from "vue"
 	import { useTicTacToeStore } from "../stores/ticTacToeStore"
+	 import { useTicTacToeHelpers } from "../composables/tttHelper";
 
+	const { checkIsSpectator } = useTicTacToeHelpers()
 	const ticTacToeStore = useTicTacToeStore()
 
 	const chatHistory = ref([""])
 	const currentMessage = ref("")
+
+	interface ChatMessage { message: string; sender_id: string;}
+	const chatHistoryAdvanced = ref(Array<ChatMessage>())
 
 	const sendMessage = (message: string) => {
 		ticTacToeStore.matchChannel?.push("broadcast_message", { message })
@@ -30,20 +36,20 @@
 
 	onMounted(() => {
 		ticTacToeStore.matchChannel?.on("game_message", (payload: { message: string; sender_id: string }) => {
-			chatHistory.value.push(payload.message)
-      autoScroll()
+      chatHistory.value.push(payload.message)
+			chatHistoryAdvanced.value.push(payload)
+	     autoScroll()
 		})
 	})
 
 	const autoScroll = () => {
 		setTimeout(() => {
-			var objDiv = document.querySelector("#chat > div:last-of-type") // document.getElementById("your_div");
+			var objDiv = document.querySelector("#chat > div:last-of-type")
 			if (objDiv) {
 				objDiv.scrollIntoView()
 			}
 		}, 250)
 	}
-
 </script>
 
 <style>
